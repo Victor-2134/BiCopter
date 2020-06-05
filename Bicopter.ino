9c614f845a8cf94597504b500a00e5ec58d8bf31
Vishnu Kumar <vishnukumar.tv@gmail.com>
2020-06-05 22:28:38 +0530

    Update Bicopter.ino
    made port changes
    removed variables not used
    made the motor to turn off if the POT is <~50
    need to rewrite the motor speed calculation

--- Bicopter.ino
+++ Bicopter.ino
@@ -1,33 +1,29 @@
 #include<Servo.h>
 
 Servo SL,SR;
-const int PS[2] = {6,7};
+const int PS[2] = {0,0};
 const int PJx = A0;
 const int PJy = A1;
-const int Pc = 1; 
-const int M[2][3] = {{11,10,3},{0,0,0}};       //col - {ip1, ip2, en}, row - {Left Motor,Right Motor}
-//const int BaseSpeed =  175;
+const int M[2][3] = {{6,9,2},{13,12,10}};       //col - {ip1, ip2, en}, row - {Left Motor,Right Motor}
 const int BaseAngle =  90;
-const int Button[2] = {4,3};
-const int Pot = ;
+const int Pot = A2;
 
 int Mspeed[2] = {0,0};
 int yangle = 0;
 int Jx = 0;
 int Jy = 0;
-int count = 0;
 int DEAD_ZONE = 5; //Joystick dead zone, gives a tiny null area in the middle of the stick to stop ghost values.
 int nullX = 500; //average output of X axis at center
 int nullY = 528; //average output of Y axis at center
 int moveAmt[2] = {0,0}; //Movement of joystick along X,Y axis
 int Speed_Base = 0;
+int l;  //to turn on/off the motor according to the altitude pot
 bool b;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(PJx,INPUT);
 pinMode(PJy,INPUT);
-pinMode(Pc,INPUT);
 pinMode(Pot,INPUT);
 //pinMode(Button[0],INPUT);
 //pinMode(Button[1],INPUT);
@@ -39,18 +35,14 @@ for(int i=0;i<2;i++){
 SL.attach(PS[0]);
 SR.attach(PS[1]);
 
-
-  //Propeller motors are made to spin in opposite direction.
-  // Left - clk, Right - anti-clk
-  
-  digitalWrite(M[0][0],HIGH);
-  digitalWrite(M[0][1],LOW);
-  digitalWrite(M[1][0],LOW);
-  digitalWrite(M[1][1],HIGH);
-  SL.write(0);
-  SR.write(0);
-  SL.write(BaseAngle);
-  SR.write(BaseAngle);
+SL.write(0);
+delay(150);
+SR.write(0);
+delay(150);
+SL.write(BaseAngle);
+delay(150);
+SR.write(BaseAngle);
+delay(150);
   
 Serial.begin(9600);
 }
@@ -80,6 +72,13 @@ moveAmt[1] = -1 * constrain((nullY - Jy), 0, 495);
 else
   moveAmt[1] = 0;
 
+// control left right movement or x - axis movement
+
+Mspeed[1] = Speed_Base
+Mspeed[0] = 
+
+// control forward, backward movement or y - axis movement
+yangle = BaseAngle + 
 }
 
 void loop() {
@@ -87,24 +86,27 @@ void loop() {
 Jx = analogRead(PJx);
 Jy = analogRead(PJy);
 Speed_Base = analogRead(Pot);
-
+l = map(Speed_Base,0,50,0,1);
+/*
+while(Speed_Base == 0){
+  Serial.print("Return");
+  goto Skip;
+}*/
+//Propeller motors are made to spin in opposite direction.
+// Left - clk, Right - anti-clk
+  
+digitalWrite(M[0][0],l);
+digitalWrite(M[0][1],LOW);
+digitalWrite(M[1][0],LOW);
+digitalWrite(M[1][1],l);
+  
+  
 Speed_Base = map(Speed_Base,0,1023,0,255);
 
 Direction();
-/*
-if(digitalRead(Button[0]))
-  Serial.println("Rise()");
-else if (digitalRead(Button[1]))
-  Serial.println("Fall()");
-*/
-// control left right movement or x - axis movement
 
-Mspeed[1] = constrain((Speed_Base[0] + moveAmt[0]),0,255);
-Mspeed[0] = 255 - Mspeed[1];
-
-// control forward, backward movement or y - axis movement
-yangle = BaseAngle + map(moveAmt[1], -495, 495, -90, 90);
-
+Serial.print(Speed_Base);
+Serial.print("  ");
 Serial.print(moveAmt[0]);
 Serial.print("  ");
 Serial.print(moveAmt[1]);
@@ -128,5 +130,6 @@ Serial.print("  ");
 Serial.println(Jy);
 //Serial.print("  ");
 //Serial.println(Mspeed);
+Skip:
 delay(100);
 }
